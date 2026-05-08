4758ee8ca683312a33667ca21ceee4e1b563cf3a
leechaeyul <ci667702@gmail.com>
2026-05-08 09:35:54 +0900

    commmit

--- main.py
+++ main.py
@@ -1,36 +1,6 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout
-
-class Character: ## 캐릭터 기본 설정
-    def __init__(self, age, name, communicationCount):
-        self.age = age
-        self.name= name
-        self.communicationCount = communicationCount
-
-    def communication(self): ## 여기서 대화 횟수를 확인하고 answer 함수로 넘어감
-        if self.communicationCount>0:
-            self.communicationCount-=1
-            return self.answer()
-        return self.answer_Fail()
-
-    def answer(self):
-        pass
-        ## gemini 연결해서 창에 띄우기
-
-    def answer_Fail(self):
-        pass
-        ## 오늘은 더이상 대화할 수 없습니다.<이런 안내멘트 띄워야함!
-        
-
-class butterfly(Character): ## 캐릭터 나비 가면 아직 구현 안됨
-    def __init__(self, age, name, communicationCount):
-        super().__init__(age, name, communicationCount)
-
-
-class volt(Character): ## 캐릭터 볼트 아직 구현 안됨
-    def __init__(self, age, name, communicationCount):
-        super().__init__(age, name, communicationCount)
-
+from CHARACTER import butterfly, volt
 
 class App(QMainWindow):
     def __init__(self):
