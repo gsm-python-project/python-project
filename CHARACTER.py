bd45f8991ed4687cc55f6972d1c33991969fc2cc
leechaeyul <ci667702@gmail.com>
2026-05-12 18:25:44 +0900

    character

--- CHARACTER.py
+++ CHARACTER.py
@@ -1,4 +1,7 @@
 
+from PyQt6.QtWidgets import *
+import sys
+
 class Character: ## 캐릭터 기본 설정
     def __init__(self, age, name, communicationCount):
         self.age = age
@@ -17,25 +20,58 @@ class Character: ## 캐릭터 기본 설정
         ## api key 받아와야함
 
     def answer_Fail(self):
-        pass
+        self.w=Answer_Fail()
+        self.w.exec()
         ## 오늘은 더이상 대화할 수 없습니다.<이런 안내멘트 띄워야함!
 
 class butterfly(Character): ## 캐릭터 나비 가면 아직 구현 안됨
     def __init__(self, age=26, name="butterfly", communicationCount=5):
         super().__init__(age, name, communicationCount)
 
+    def answer(self):
+        print("butterfly")
+
 class volt(Character): ## 캐릭터 볼트 아직 구현 안됨
     def __init__(self, age=26, name="volt", communicationCount=5):
         super().__init__(age, name, communicationCount)
+
+    def answer(self):
+        print("volt")
 
 class colombina(Character):
     def __init__(self, age=27, name="colombina", communicationCount=5):
         super().__init__(age, name, communicationCount)
 
+    def answer(self):
+        print("colombina")
+
 class crow(Character):
     def __init__(self, age=37, name="crow", communicationCount=3):
         super().__init__(age, name, communicationCount)
+
+    def answer(self):
+        print("crow")
     
 class moreta(Character):
     def __init__(self, age=18, name="moreta", communicationCount=3):
         super().__init__(age, name, communicationCount)
+
+    def answer(self):
+        print("test")
+
+class Answer_Fail(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle("")
+        self.resize(300,150)
+
+        layout = QVBoxLayout()
+        self.label = QLabel("오늘은 더이상 대화할 수 없습니다.")
+        self.btn_close = QPushButton("닫기")
+        self.btn_close.resize(20,50)
+        self.btn_close.clicked.connect(self.close)
+
+        layout.addWidget(self.label)
+        layout.addWidget(self.btn_close)
+        self.setLayout(layout)
